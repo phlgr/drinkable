d69df62f144201ce3565b23abf7965db9e442b1c
phlgr <philippm.gartz@gmail.com>
2020-04-09 15:26:01 +0200

    Add light Theme and GlobalStyles

---
+++ client/src/themes/GlobalStyles.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Global, css } from '@emotion/core';
+
+export default function GlobalStyles() {
+  return (
+    <Global
+      styles={(theme) => css`
+        @import url('https://fonts.googleapis.com/css2?family=Comfortaa:wght@300;400;700&display=swap');
+        *,
+        *::after,
+        *::before {
+          box-sizing: border-box;
+        }
+        body {
+          font-family: Comfortaa;
+          font-size: 16px;
+          margin: 0;
+          background: ${theme.background};
+          height: 100vh;
+          width: 100vw;
+        }
+      `}
+    />
+  );
+}
