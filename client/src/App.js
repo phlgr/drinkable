7ddd214f40cbba9f7cb3d03211464e1041936be6
phlgr <philippm.gartz@gmail.com>
2020-04-09 18:15:15 +0200

    Add ThemeProvider, GlobalStyles and React Router

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,25 @@
 import React from 'react';
+import GlobalStyles from './themes/GlobalStyles';
+import { ThemeProvider } from 'emotion-theming';
+import { light } from './themes/light.js';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import Home from './pages/Home';
 
 function App() {
-  return <p>Test</p>;
+  const [theme] = React.useState(light);
+  return (
+    <Router>
+      <ThemeProvider theme={theme}>
+        <GlobalStyles />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </ThemeProvider>
+    </Router>
+  );
 }
 
 export default App;
