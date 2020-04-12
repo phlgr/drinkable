46d0928e1a0f63992ad602a047dc805232a71abd
phlgr <philippm.gartz@gmail.com>
2020-04-12 14:51:50 +0200

    Fix storybook file name

---
+++ client/src/stories/1-Header.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Header from '../components/Header';
+
+export default {
+  title: 'Header',
+  component: Header,
+};
+
+export const StandardHeader = () => <Header />;
