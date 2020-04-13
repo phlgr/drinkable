b6746307f2b6042a5f6ba01539bf0b73fc68ee28
phlgr <philippm.gartz@gmail.com>
2020-04-13 12:21:21 +0200

    Add Ingredient Selector to storybook

---
+++ client/src/components/Ingredient.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import styled from '@emotion/styled';
+
+const IngredientSelector = styled.button`
+  font-family: Comfortaa;
+  color: ${(props) => props.theme.secondaryActive};
+  padding: 4px 10px;
+  font-size: 1.2rem;
+  border: 3px solid ${(props) => props.theme.secondary};
+  border-radius: 10px;
+  background: none;
+  outline: none;
+  ${(props) =>
+    props.selected
+      ? `
+  transition: 2s;
+  color: #fff;
+  border: 3px solid ${(props) => props.theme.secondaryActive};
+  background: ${(props) => props.theme.secondaryActive}
+  `
+      : ''}
+`;
+
+export default function Ingredient(props) {
+  return (
+    <>
+      <IngredientSelector {...props}></IngredientSelector>
+    </>
+  );
+}
