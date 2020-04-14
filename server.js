45ec2212d059e9dbd18b4c3f9d9a0fc6fd4e7b1a
phlgr <philippm.gartz@gmail.com>
2020-04-14 17:53:30 +0200

    Create new route to /api/ingredients

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const path = require('path');
 
+const ingredients = require('./lib/routes/ingredients');
+
 const port = process.env.PORT || 8080;
 const app = express();
+
+app.use(express.json());
+
+app.use('/api/ingredients', ingredients);
 
 app.use(express.static(path.join(__dirname, 'client/build')));
 app.get('*', (request, response) => {
   response.sendFile(path.join(__dirname, 'client/build/index.html'));
 });
+
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
