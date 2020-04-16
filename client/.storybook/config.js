24e95ff55e40b85def2f649a121aa8ca2328fc61
phlgr <philippm.gartz@gmail.com>
2020-04-16 19:59:52 +0200

    Fix GlobalStyles

--- client/.storybook/config.js
+++ client/.storybook/config.js
@@ -1,10 +1,10 @@
 import { addDecorator } from '@storybook/react';
-import themeDecorator from './themeDecorator';
+import ThemeDecorator from './ThemeDecorator';
 import { addParameters } from '@storybook/react';
 import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
-
-addDecorator(themeDecorator);
 
 addParameters({
   viewport: { viewports: INITIAL_VIEWPORTS, defaultViewport: 'iphone6' },
 });
+
+addDecorator(ThemeDecorator);
