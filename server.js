5d077ce9f5a99228b44f8182de310181bb8990f0
phlgr <philippm.gartz@gmail.com>
2020-04-16 12:56:11 +0200

    Import dotenv

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+require('dotenv').config();
 
 const ingredients = require('./lib/routes/ingredients');
 const drinks = require('./lib/routes/drinks');
