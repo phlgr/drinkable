14b1beaa98843ba11d1f8590cd7b18e6866e9382
phlgr <philippm.gartz@gmail.com>
2020-04-17 17:58:24 +0200

    Create getIngredients hook

---
+++ client/src/hooks/useGetIngredients.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { getIngredients } from '../api/ingredients';
+
+export default function useGetIngredients() {
+  const [ingredients, setIngredients] = React.useState(null);
+  const [error, setError] = React.useState(false);
+  const [loading, setLoading] = React.useState(true);
+
+  async function doGetIngredients() {
+    try {
+      setLoading(true);
+      setError(false);
+      const ingredients = await getIngredients();
+      setIngredients(ingredients);
+    } catch (error) {
+      setError(true);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  React.useEffect(() => {
+    doGetIngredients();
+  }, []);
+
+  return [{ ingredients, error, loading }, doGetIngredients];
+}
