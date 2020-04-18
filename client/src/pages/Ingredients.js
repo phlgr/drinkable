c29376b4b925a3a486efd01490f15cb09177fb65
phlgr <philippm.gartz@gmail.com>
2020-04-18 14:56:42 +0200

    Insert Ingredients into page

--- client/src/pages/Ingredients.js
+++ client/src/pages/Ingredients.js
@@ -1,11 +1,30 @@
 import React from 'react';
 
 import PartyContainer from '../components/PartyContainer';
+import SearchInput from '../components/SearchInput';
+import Loading from '../components/Loading';
+
+import useGetIngredients from '../hooks/useGetIngredients';
 
 export default function Ingredients() {
+  const [searchValue, setSearchValue] = React.useState('');
+  const [{ ingredients, error, loading }] = useGetIngredients();
+
+  function handleChange(value) {
+    setSearchValue(value);
+  }
   return (
     <>
-      <PartyContainer></PartyContainer>
+      <PartyContainer>
+        <SearchInput
+          value={searchValue}
+          onChange={(event) => handleChange(event.target.value)}
+          placeholder={'Search ingredients'}
+        ></SearchInput>
+        {loading && <Loading />}
+        {error && <p>Error!</p>}
+        {ingredients && ingredients}
+      </PartyContainer>
     </>
   );
 }
