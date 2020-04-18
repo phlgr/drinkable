c392255055ad82dbfa526d71ab05430e7c1bf44a
phlgr <philippm.gartz@gmail.com>
2020-04-18 11:40:38 +0200

    Add PartyContainer storybook

---
+++ client/src/stories/9-PartyContainer.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import PartyContainer from '../components/PartyContainer';
+
+export default {
+  title: 'PartyContainer',
+  component: PartyContainer,
+};
+
+export const ContainerEmpty = () => <PartyContainer>w</PartyContainer>;
