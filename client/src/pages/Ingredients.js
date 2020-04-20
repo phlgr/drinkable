d641b618f2f86270ee5d0af7107e2e7dc3137884
phlgr <philippm.gartz@gmail.com>
2020-04-20 18:52:25 +0200

    Save chosen ingredients in Array

--- client/src/pages/Ingredients.js
+++ client/src/pages/Ingredients.js
@@ -8,14 +8,41 @@ import useGetIngredients from '../hooks/useGetIngredients';
 
 export default function Ingredients() {
   const [searchValue, setSearchValue] = React.useState('');
-  const [{ ingredients, error, loading }] = useGetIngredients(searchValue);
+  const [selectedIngredients, setSelectedIngredients] = React.useState([]);
+
+  const handleSelect = (name) => {
+    console.log(selectedIngredients);
+    const newIngredients = selectedIngredients;
+    if (selectedIngredients.includes(name)) {
+      const itemIndex = newIngredients.indexOf(name);
+      newIngredients.splice(itemIndex, 1);
+    } else {
+      newIngredients.push(name);
+    }
+    setSelectedIngredients(newIngredients);
+  };
+
+  const [{ ingredients, error, loading }] = useGetIngredients(
+    searchValue,
+    handleSelect
+  );
+
+  function handleButtonClick() {
+    console.log('You clicked me!');
+  }
 
   function handleChange(value) {
     setSearchValue(value);
   }
+
   return (
     <>
-      <PartyContainer>
+      <PartyContainer
+        button={{
+          label: 'Continue',
+          onClick: handleButtonClick,
+        }}
+      >
         <SearchInput
           value={searchValue}
           onChange={(event) => handleChange(event.target.value)}
