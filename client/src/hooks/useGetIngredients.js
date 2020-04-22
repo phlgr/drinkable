fbb1a2e88b74265d0fee1bc93e86b5dea99f87a9
phlgr <philippm.gartz@gmail.com>
2020-04-22 10:28:58 +0200

    Outsource component building

--- client/src/hooks/useGetIngredients.js
+++ client/src/hooks/useGetIngredients.js
@@ -1,22 +1,15 @@
 import React from 'react';
 import { getIngredients } from '../api/ingredients';
 
-import Ingredient from '../components/Ingredient';
-
-export default function useGetIngredients(searchValue, handleSelect) {
+export default function useGetIngredients(searchValue) {
   const [ingredients, setIngredients] = React.useState(null);
   const [error, setError] = React.useState(false);
   const [loading, setLoading] = React.useState(true);
 
   async function doGetIngredients() {
     try {
-      const ingredients = await getIngredients(searchValue);
-      const ingredientSelectors = ingredients.map((ingredient) => (
-        <Ingredient onSelect={handleSelect} key={ingredient.name}>
-          {ingredient.name}
-        </Ingredient>
-      ));
-      setIngredients(ingredientSelectors);
+      const filteredIngredients = await getIngredients(searchValue);
+      setIngredients(filteredIngredients);
     } catch (error) {
       setError(true);
     } finally {
