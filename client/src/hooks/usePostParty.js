df358dfd95133e60065bfdf2095fd6c0b0c1be29
phlgr <philippm.gartz@gmail.com>
2020-04-22 17:56:13 +0200

    Create Hook to create Party

---
+++ client/src/hooks/usePostParty.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { postParty } from '../api/party';
+
+export default function usePostParty() {
+  const [partyId, setPartyId] = React.useState(null);
+  const [error, setError] = React.useState(false);
+  const [loading, setLoading] = React.useState(false);
+
+  async function doPost() {
+    try {
+      setLoading(true);
+      const partyId = await postParty();
+      setPartyId(partyId);
+    } catch (error) {
+      console.error.apply(error);
+      setError(true);
+    } finally {
+      setLoading(false);
+    }
+  }
+  return [{ partyId, error, loading }, doPost];
+}
