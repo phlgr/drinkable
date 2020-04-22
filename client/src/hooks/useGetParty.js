3fc6fd29c6d308673ac2d9bebd86568a308ad274
phlgr <philippm.gartz@gmail.com>
2020-04-22 18:02:17 +0200

    Create Hook to get Party

---
+++ client/src/hooks/useGetParty.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { getParty } from '../api/party';
+
+export default function useGetParty(partyId) {
+  const [party, setParty] = React.useState(null);
+  const [error, setError] = React.useState(false);
+  const [loading, setLoading] = React.useState(false);
+
+  async function doGet() {
+    try {
+      setLoading(true);
+      const party = await getParty(partyId);
+      setParty(party);
+    } catch (error) {
+      console.error(error);
+      setError(true);
+    } finally {
+      setLoading(false);
+    }
+  }
+  React.useEffect(() => {
+    doGet();
+  }, [partyId]);
+
+  return [{ party, error, loading }];
+}
