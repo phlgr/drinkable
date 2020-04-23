3baf10605f7d131ad6dc126b373d4b992c356232
phlgr <philippm.gartz@gmail.com>
2020-04-23 16:11:00 +0200

    Add Input filed and Save checkmark

--- client/src/components/PartyName.js
+++ client/src/components/PartyName.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import styled from '@emotion/styled';
+import PropTypes from 'prop-types';
 
 import Edit from '../assets/pencil-icon.svg';
+import Save from '../assets/checkmark.svg';
 
 const PartyNameContainer = styled.div`
   display: flex;
@@ -15,15 +17,48 @@ const Name = styled.h2`
 `;
 const EditButton = styled.img`
   padding: 10px;
+  cursor: pointer;
+`;
+const PartyNameInput = styled.input`
+  color: ${(props) => props.theme.secondaryActive};
+  max-width: 60%;
+  background: none;
+  font-family: Comfortaa;
+  font-size: 2.3rem;
+  font-weight: 600;
+  margin: 20px 0;
+  border: none;
+  border-bottom: 3px solid ${(props) => props.theme.secondaryActive};
 `;
 
-export default function PartyName() {
+export default function PartyName({ partyName }) {
+  const [edit, setEdit] = React.useState(false);
+
+  function handleEditClick() {
+    setEdit(true);
+  }
+
+  function handleSaveClick() {}
   return (
     <>
       <PartyNameContainer>
-        <Name>Your Party</Name>
-        <EditButton src={Edit} alt="Edit Icon" />
+        {!edit && (
+          <>
+            <Name>{partyName}</Name>
+            <EditButton onClick={handleEditClick} src={Edit} alt="Edit Icon" />
+          </>
+        )}
+        {edit && (
+          <>
+            <PartyNameInput placeholder={partyName} />
+            <EditButton onClick={handleSaveClick} src={Save} />
+          </>
+        )}
       </PartyNameContainer>
     </>
   );
 }
+
+PartyName.propTypes = {
+  partyName: PropTypes.string,
+};
