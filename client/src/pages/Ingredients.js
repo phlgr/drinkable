2117858e0b59e8125e4e1ca60f8e604604597182
phlgr <philippm.gartz@gmail.com>
2020-04-23 16:12:10 +0200

    Load name out of DB on page load

--- client/src/pages/Ingredients.js
+++ client/src/pages/Ingredients.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
 import PartyContainer from '../components/PartyContainer';
 import SearchInput from '../components/SearchInput';
@@ -6,11 +7,14 @@ import Loading from '../components/Loading';
 import Ingredient from '../components/Ingredient';
 
 import useGetIngredients from '../hooks/useGetIngredients';
+import useGetParty from '../hooks/useGetParty';
 
 export default function Ingredients() {
+  const { id } = useParams();
   const [searchValue, setSearchValue] = React.useState('');
   const [selectedIngredients, setSelectedIngredients] = React.useState([]);
   const [{ ingredients, error, loading }] = useGetIngredients(searchValue);
+  const [{ party, getError, getLoading }] = useGetParty(id);
 
   const handleSelect = (name) => {
     const newIngredients = selectedIngredients;
@@ -34,29 +38,34 @@ export default function Ingredients() {
 
   return (
     <>
-      <PartyContainer
-        button={{
-          label: 'Continue',
-          onClick: handleButtonClick,
-        }}
-      >
-        <SearchInput
-          value={searchValue}
-          onChange={(event) => handleChange(event.target.value)}
-          placeholder={'Search ingredients'}
-        ></SearchInput>
-        {loading && <Loading />}
-        {error && <p>Error!</p>}
-        {ingredients &&
-          ingredients.map((ingredient) => (
-            <Ingredient
-              onSelect={() => handleSelect(ingredient.name)}
-              key={ingredient.name}
-            >
-              {ingredient.name}
-            </Ingredient>
-          ))}
-      </PartyContainer>
+      {getError && 'Error!'}
+      {getLoading && <Loading />}
+      {party && (
+        <PartyContainer
+          button={{
+            label: 'Continue',
+            onClick: handleButtonClick,
+          }}
+          partyName={{ name: party.name }}
+        >
+          <SearchInput
+            value={searchValue}
+            onChange={(event) => handleChange(event.target.value)}
+            placeholder={'Search ingredients'}
+          ></SearchInput>
+          {loading && <Loading />}
+          {error && <p>Error!</p>}
+          {ingredients &&
+            ingredients.map((ingredient) => (
+              <Ingredient
+                onSelect={() => handleSelect(ingredient.name)}
+                key={ingredient.name}
+              >
+                {ingredient.name}
+              </Ingredient>
+            ))}
+        </PartyContainer>
+      )}
     </>
   );
 }
