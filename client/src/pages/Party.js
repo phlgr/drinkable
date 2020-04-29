fe118e09135dd88722f43f57a9b51105cc451798
phlgr <philippm.gartz@gmail.com>
2020-04-29 20:46:02 +0200

    Implement hook into Party Page

--- client/src/pages/Party.js
+++ client/src/pages/Party.js
@@ -2,13 +2,16 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 import styled from '@emotion/styled';
 
-import PartyContainer from '../components/PartyContainer';
 import useGetParty from '../hooks/useGetParty';
+import useGetPartyDrinks from '../hooks/useGetPartyDrinks';
+
+import PartyContainer from '../components/PartyContainer';
 import Loading from '../components/Loading';
 import Thumbnail from '../components/Thumbnail';
 import SearchInput from '../components/SearchInput';
 
 const ThumbnailContainer = styled.div`
+  width: 100%;
   display: flex;
   flex-flow: row wrap;
   justify-content: space-between;
@@ -18,6 +21,9 @@ export default function Party() {
   const { id } = useParams();
   const [searchValue, setSearchValue] = React.useState('');
   const [{ party, error, loading }, doGetParty] = useGetParty(id);
+  const [
+    { drinks, error: errorDrinks, loading: loadingDrinks },
+  ] = useGetPartyDrinks(id, searchValue);
   return (
     <>
       {error && <p>No Party found :(</p>}
@@ -36,11 +42,15 @@ export default function Party() {
             value={searchValue}
             onChange={(event) => setSearchValue(event.target.value)}
           />
-          <ThumbnailContainer>
-            {Object.entries(party.drinks).map(([key, value]) => (
-              <Thumbnail key={value.id} src={value.thumbnail} name={key} />
-            ))}
-          </ThumbnailContainer>
+          {errorDrinks && <p>Could not load drinks :(</p>}
+          {loadingDrinks && <Loading />}
+          {drinks && !loadingDrinks && (
+            <ThumbnailContainer>
+              {Object.entries(drinks).map(([key, value]) => (
+                <Thumbnail key={value.id} src={value.thumbnail} name={key} />
+              ))}
+            </ThumbnailContainer>
+          )}
         </PartyContainer>
       )}
     </>
