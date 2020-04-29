32953c1861b7a3fbdbf638dc726441f21e7a490a
phlgr <philippm.gartz@gmail.com>
2020-04-30 00:45:43 +0200

    Create admin route to refresh drinks in DB

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const { setIngredientsDB } = require('./lib/models/ingredients');
 const ingredients = require('./lib/routes/ingredients');
 const drinks = require('./lib/routes/drinks');
 const parties = require('./lib/routes/parties');
+const admin = require('./lib/routes/admin');
 
 const port = process.env.PORT || 8080;
 const app = express();
@@ -17,6 +18,7 @@ app.use(express.json());
 app.use('/api/ingredients', ingredients);
 app.use('/api/drinks', drinks);
 app.use('/api/parties', parties);
+app.use('/api/admin', admin);
 
 app.use(express.static(path.join(__dirname, 'client/build')));
 app.get('*', (request, response) => {
