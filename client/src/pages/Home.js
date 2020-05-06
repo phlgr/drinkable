d3e395a5cddc28b59d922e4257268ba0f97da32f
phlgr <philippm.gartz@gmail.com>
2020-05-06 13:55:37 +0200

    Add Instructions to homepage

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -9,6 +9,14 @@ import Button from '../components/Button';
 import backgroundImage from '../assets/home-background.png';
 import Loading from '../components/Loading';
 
+const BorderContainer = styled.div`
+  position: fixed;
+  display: flex;
+  flex-flow: column nowrap;
+  width: 100%;
+  height: 100%;
+`;
+
 const PrimaryContainer = styled.div`
   background-image: url("${backgroundImage}");
   background-size: cover;
@@ -29,6 +37,38 @@ const InfoText = styled.div`
   margin: 20px 0 60px;
 `;
 
+const SecondaryContainer = styled.div`
+  display: flex;
+  flex-flow: column wrap;
+  background: ${(props) => props.theme.secondaryActive};
+  color: white;
+  padding: 40px 20px 60px;
+  flex-grow: 1;
+  align-items: center;
+  h2 {
+    margin: 0;
+  }
+  li {
+    padding: 5px 0;
+  }
+  ol {
+    margin-bottom: 30px;
+  }
+  Button {
+    align-self: flex-end;
+  }
+`;
+
+const InstructionContainer = styled.div``;
+
+const Footer = styled.footer`
+  background: ${(props) => props.theme.background};
+  width: 100%;
+  text-align: center;
+  padding: 3px 0;
+  font-size: 0.8rem;
+`;
+
 export default function Home() {
   const [{ partyId, error, loading }, doPost] = usePostParty();
   const history = useHistory();
@@ -43,7 +83,7 @@ export default function Home() {
     doPost();
   }
   return (
-    <>
+    <BorderContainer>
       <PrimaryContainer>
         <Header />
         <InfoText>
@@ -55,6 +95,23 @@ export default function Home() {
           {error && 'Try again!'}
         </Button>
       </PrimaryContainer>
-    </>
+      <SecondaryContainer>
+        <InstructionContainer>
+          <h2>How to use:</h2>
+          <ol>
+            <li>Add Ingredients to your party</li>
+            <li>Name your Party</li>
+            <li>Share your Party with friends</li>
+            <li>Have Fun!</li>
+          </ol>
+        </InstructionContainer>
+        <Button background="secondary" onClick={partyButtonHandleCLick}>
+          {!loading && !error && 'Start Planning!'}
+          {loading && <Loading white />}
+          {error && 'Try again!'}
+        </Button>
+      </SecondaryContainer>
+      <Footer>Every Button leads towards the party!</Footer>
+    </BorderContainer>
   );
 }
