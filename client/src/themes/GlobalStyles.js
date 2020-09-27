f7f843bfca768e041c1dff61e979272578109920
phlgr <philippm.gartz@gmail.com>
2020-09-27 19:49:56 +0200

    Create media query varibales

--- client/src/themes/GlobalStyles.js
+++ client/src/themes/GlobalStyles.js
@@ -11,6 +11,7 @@ export default function GlobalStyles() {
         *::before {
           box-sizing: border-box;
         }
+
         body {
           font-family: 'Comfortaa';
           font-size: 16px;
@@ -23,3 +24,21 @@ export default function GlobalStyles() {
     />
   );
 }
+
+const bp = {
+  sm: 576,
+  md: 768,
+  lg: 992,
+  xl: 1200,
+};
+
+export const mq = (n) => {
+  const bpArray = Object.keys(bp).map((key) => [key, bp[key]]);
+
+  const [result] = bpArray.reduce((acc, [name, size]) => {
+    if (n === name) return [...acc, `@media (min-width: ${size}px)`];
+    return acc;
+  }, []);
+
+  return result;
+};
